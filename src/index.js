bc4cabbba778f6a221d39853f591c166dcc8e463
oktay <oktaycolakoglu@gmail.com>
2021-07-04 13:48:56 +0300

    ie11 support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 
 ReactDOM.render(<App />, document.getElementById('root'))
